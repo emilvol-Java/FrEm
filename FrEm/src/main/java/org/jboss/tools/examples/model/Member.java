b7af2f47fd3fb7a891b9e8c4361d298c76be9156
oddfredrikhansen <pappafredrik67@hotmail.se>
2017-06-11 22:44:15 +0200

    dfd

--- FrEm/src/main/java/org/jboss/tools/examples/model/Member.java
+++ FrEm/src/main/java/org/jboss/tools/examples/model/Member.java
@@ -19,6 +19,7 @@ package org.jboss.tools.examples.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -37,6 +38,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
+import org.jboss.tools.examples.data.DogRepository;
 
 @SuppressWarnings("serial")
 @Entity
@@ -94,6 +96,17 @@ public class Member implements Serializable {
 	public void addDogToList(Dog doggy){
 		dogList.add(doggy);
 	}
+//	@Inject 
+//	DogRepository dogrepo;
+//	
+//    public void retrieveAllDogsByEmail() {
+//        dogList = dogrepo.findAllOrderedByName();
+//        for(Dog dd:dogList){
+//        	if(!dd.getOwner().equals(this.email)){
+//        		dogList.remove(dd);
+//        	}
+//        }
+//    }
 	
 	public void removeDogFromList(Dog doggy){
 		
